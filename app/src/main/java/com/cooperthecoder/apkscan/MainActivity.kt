604ac2e3cc7d2ccf1c01839fdfd7cfb0a705ba52
Jonathan Cooper <cooper@cooperthecoder.com>
2020-06-09 16:11:02 -0500

    Added RxAndroid

--- app/src/main/java/com/cooperthecoder/apkscan/MainActivity.kt
+++ app/src/main/java/com/cooperthecoder/apkscan/MainActivity.kt
@@ -4,18 +4,27 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val pm = applicationContext.packageManager
-
-        val packages = pm.getInstalledApplications(PackageManager.GET_META_DATA)
         val adapter = PackagesListAdapter(this)
         packagesList.adapter = adapter
         packagesList.layoutManager = LinearLayoutManager(this)
-        adapter.addPackages(packages)
+
+        Flowable.fromCallable {
+            val pm = applicationContext.packageManager
+            return@fromCallable pm.getInstalledApplications(PackageManager.GET_META_DATA)
+        }
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { packages ->
+                adapter.addPackages(packages)
+            }
     }
 }
--- app/src/main/java/com/cooperthecoder/apkscan/ScanResultsActivity.kt
+++ app/src/main/java/com/cooperthecoder/apkscan/ScanResultsActivity.kt
@@ -3,6 +3,9 @@ package com.cooperthecoder.apkscan
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_scan_results.*
 
 class ScanResultsActivity : AppCompatActivity() {
@@ -17,20 +20,27 @@ class ScanResultsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_scan_results)
         val packageName = intent.getStringExtra(PACKAGE_NAME)
 
-        // TODO: Do this in background using RxAndroid
-        val pm = applicationContext.packageManager
-        val flags = PackageManager.GET_SHARED_LIBRARY_FILES or PackageManager.GET_META_DATA
-        val applicationInfo = pm.getApplicationInfo(packageName, flags)
-        val sb = StringBuilder()
-        applicationInfo.sharedLibraryFiles.forEach {
-            sb.append(it)
+        Flowable.fromCallable {
+            val pm = applicationContext.packageManager
+            val flags = PackageManager.GET_SHARED_LIBRARY_FILES or PackageManager.GET_META_DATA
+            return@fromCallable pm.getApplicationInfo(packageName, flags)
         }
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { applicationInfo ->
+                val sb = StringBuilder()
+                applicationInfo.sharedLibraryFiles.forEach {
+                    sb.append(it)
+                }
 
-        // TODO: Display app info in user-friendly way
-        val applicationInfoData = "Package name: ${applicationInfo.packageName}\n" +
-                "Data directory: ${applicationInfo.dataDir}\n" +
-                "Shared libraries: $sb"
+                // TODO: Display more info from app
+                val applicationInfoData = "Package name: ${applicationInfo.packageName}\n" +
+                        "Data directory: ${applicationInfo.dataDir}\n" +
+                        "Shared libraries: $sb"
+                applicationInfoText.text = applicationInfoData
+            }
 
-        applicationInfoText.text = applicationInfoData
+
+
     }
 }
