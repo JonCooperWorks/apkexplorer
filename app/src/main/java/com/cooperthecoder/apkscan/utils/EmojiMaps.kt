e60acbc1e045201a8c8974ba2539252fd50177f7
Jonathan Cooper <cooper@cooperthecoder.com>
2020-06-25 19:43:09 -0500

    Added FLAG_USES_CLEARTEXT_TRAFFIC to emoji map

--- app/src/main/java/com/cooperthecoder/apkscan/utils/EmojiMaps.kt
+++ app/src/main/java/com/cooperthecoder/apkscan/utils/EmojiMaps.kt
@@ -3,6 +3,7 @@ package com.cooperthecoder.apkscan.utils
 object EmojiMaps {
     val ACTIVITIES = mapOf(
         Pair("FLAG_DEBUGGABLE", "⚠️"),
-        Pair("FLAG_ALLOW_BACKUP", "⚠️")
+        Pair("FLAG_ALLOW_BACKUP", "⚠️"),
+        Pair("FLAG_USES_CLEARTEXT_TRAFFIC", "⚠️")
     )
 }
